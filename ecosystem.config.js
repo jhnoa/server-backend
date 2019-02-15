5356dec2b863261af8ff23032a35b25225f885ff
AttreaVe <johansiautama@gmail.com>
2019-02-16 00:10:14 +0700

    update ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,30 +1,36 @@
 module.exports = {
-  apps : [{
-    name: 'API',
-    script: 'app.js',
+  apps: [
+    {
+      name: 'API',
+      script: 'npm',
 
-    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-    args: 'one two',
-    instances: 4,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '240M',
-    env: {
-      NODE_ENV: 'development'
+      // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+      exec_mode: 'cluster',
+      instances: 'max',
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '240M',
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+      node_args: 'start',
     },
-    env_production: {
-      NODE_ENV: 'production'
-    }
-  }],
+  ],
 
-  deploy : {
-    production : {
-      user : 'node',
-      host : '212.83.163.1',
-      ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/production',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
-    }
-  }
+  deploy: {
+    production: {
+      user: 'node',
+      host: '212.83.163.1',
+      ref: 'origin/master',
+      repo: 'git@github.com:repo.git',
+      path: '/var/www/production',
+      'post-deploy':
+        'npm install && pm2 reload ecosystem.config.js --env production',
+    },
+    staging: {},
+    development: {},
+  },
 };
